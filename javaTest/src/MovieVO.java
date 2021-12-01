ac37a0d40ba487f3043fbe32ff658f5eeb80a490
kyuyoungoh <oqoq59@gmail.com>
2021-12-01 23:49:47 +0900

    overloading

---
+++ javaTest/src/MovieVO.java
@@ -0,0 +1,17 @@
+public class MovieVO {
+
+    //constructor overloading 생성자 중복정의
+    //new MovieVO();
+    //new MovieVO(title, runTime, director);
+    //객체지향에서는 메서드 이름이 같아도 에러x
+    private String title;
+    private int runTime;
+    private String director;
+
+    public MovieVO(String title, int runTime, String director) {
+        this.title = title;
+        this.runTime = runTime;
+        this.director = director;
+    }
+    
+}
