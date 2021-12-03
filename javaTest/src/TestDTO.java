7c124795a365607b38f32b3daca1f7e5e28469a1
kyuyoungoh <oqoq59@gmail.com>
2021-12-03 21:49:53 +0900

    getter and setter

--- javaTest/src/TestDTO.java
+++ javaTest/src/TestDTO.java
@@ -1,11 +1,30 @@
 public class TestDTO {
     
-    public String name;
-    public int age;
-    public String city;
+    private String name;
+    private int age;
+    private String city;
 
     //default constructor
     public TestDTO(){
         super();
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getCity() {
+        return city;
     }
 }
