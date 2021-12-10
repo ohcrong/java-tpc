9b1f07ded5edfa086bf6394a9341a2e0e3fdca1a
kyuyoungoh <oqoq59@gmail.com>
2021-12-10 22:26:11 +0900

    overrride

--- javaTest/src/App.java
+++ javaTest/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         //Override
-        
+        Sport football = new Football();
+        football.play();
     }
 }
