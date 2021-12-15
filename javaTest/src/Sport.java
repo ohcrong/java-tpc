dc7f567b968eecc9116f4324ded10080aea5f5ce
kyuyoungoh <oqoq59@gmail.com>
2021-12-15 23:59:52 +0900

    method

--- javaTest/src/Sport.java
+++ javaTest/src/Sport.java
@@ -3,6 +3,7 @@ public abstract class Sport {
     //하위 클래스와 같이 사용시 부모 역할을 하며 다형성 보장
     public abstract void play();
 
+    //공통메서드
     public void move() {
         System.out.println("a player moves to win");
     }
