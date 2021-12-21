101a37d668437220f513b10a54de0879001cc5fe
kyuyoungoh <oqoq59@gmail.com>
2021-12-21 23:55:00 +0900

    multipple implements

---
+++ javaTest/src/Ball.java
@@ -0,0 +1,3 @@
+public interface Ball {
+    public void playWithBall();
+}
--- javaTest/src/Football.java
+++ javaTest/src/Football.java
@@ -1,8 +1,16 @@
-public class Football extends Sport {
+public class Football extends Sport implements Ball, Team{
     
     @Override
     public void play() {
         System.out.println("play first half");
+    }
+    @Override
+    public void playWithBall() {
+        System.out.println("play with the ball");
+    }
+    @Override
+    public void playAsTeam() {
+        System.out.println("play as team");
     }
 
     public void cornerKick() {
